3342e12b502eb3e83a81070123441b63564339de
Ricardo A Leyva <25676407+ricardoandres9728@users.noreply.github.com>
2020-09-21 16:21:01 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/{any?}', function () {
     return view('superadmin');
-})->where('any', '^(?!storage).*$')->name('dashboard');;
+})->where('any', '^(?!storage).*$')->name('dashboard');
 
+Route::domain('{page}.myapp.com')->group(function () {
+    Route::get('/{any?}', function ($page) {
+        switch ($page) {
+            case 'dash':
+                return view('superadmin');
+                break;
+            case 'porteria':
+                return view('reception');
+                break;
+            case 'conjunto':
+                return view('adminconjuntos');
+                break;
+        }
+    });
+});
