9f3003ab0f2ef85c2b1fb6e269ca996fc806efb0
Ricardo A Leyva <25676407+ricardoandres9728@users.noreply.github.com>
2020-09-21 13:23:30 -0500

    Update AlerNotification.php

--- app/Traits/AlerNotification.php
+++ app/Traits/AlerNotification.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Log;
 use Kreait\Firebase\Exception\Messaging\NotFound;
 use Kreait\Firebase\Messaging\CloudMessage;
 use Kreait\Firebase\Messaging\Notification;
-use Kreait\Firebase\Messaging\AndroidConfig;
 
 /**
  * Notification Trait
@@ -20,16 +19,9 @@ trait AlerNotification
             $title = "Alerta de seguridad " . $complex->name;
             $body = "Se ha enviado una alerta de seguridad en";
 
-            $config = AndroidConfig::fromArray([
-                'ttl' => '3600s',
-                'priority' => 'high',
-            ]);
             $notification = Notification::fromArray([
                 'title' => $title,
                 'body' => $body,
-                "click_action" => "FCM_PLUGIN_ACTIVITY",
-                "icon" => "myicon",
-                "sound" => "default",
             ]);
             $message = CloudMessage::new();
             $message = $message->withNotification($notification)
@@ -37,8 +29,7 @@ trait AlerNotification
                     "id" => "alert",
                     "lat" => $lat,
                     "lng" => $lng
-                ])
-                ->withAndroidConfig($config);;
+                ]);
 
             $messaging->sendMulticast($message, $tokens);
         } catch (NotFound $th) {
